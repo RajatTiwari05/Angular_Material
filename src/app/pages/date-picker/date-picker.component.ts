095b0995620c360b5a74d27de2ed0528599ff345
RajatTiwari05 <rajat.tiwari5555@gmail.com>
2024-04-02 17:20:06 +0530

    commiting the Angular Material Learning changes

--- src/app/pages/date-picker/date-picker.component.ts
+++ src/app/pages/date-picker/date-picker.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatModule } from '../../appModule/mat.module';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-date-picker',
   standalone: true,
-  imports: [],
+  imports: [MatModule,MatDatepickerModule],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss'
 })
